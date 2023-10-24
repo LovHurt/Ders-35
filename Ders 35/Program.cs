06a7ac012e4ca22711c7ac194957caa449fb17ab
Alican Dağıdır <alicandagidir@hotmail.com>
2023-10-24 17:17:51 +0300

    Add project files.

---
+++ Ders 35/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "Ankara";
+
+            foreach(var item in city)
+            {
+                Console.WriteLine(item);
+            }
+            string city2 = "İstanbul";
+            string result = city + city2;
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
